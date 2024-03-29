d023b961d01237fc6b4a7775a936d380e3ff1f2f
mahendra <mahendra.syathi@ifortepay.id>
2024-03-29 23:52:40 +0700

    Init. new landing page with blog

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -2,7 +2,6 @@ import { defineConfig } from 'vocs'
 
 export default defineConfig({
   iconUrl: '/logo.png', 
-  basePath: '/docs', 
   title: 'mahendra.page',
   description: "I'm a Software Engineer. Write in Go, PHP, and sometimes JS. Love classical music.",
   theme: { 
